6a04b7cc8ad5b4b12a0152ee9dd525627306ffa3
Sumnima B <sumnimabhandari@gmail.com>
2020-06-18 16:24:35 -0700

    Added mysql connection code and created database with 3 tables

---
+++ employee_DB.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  PRIMARY KEY (id)
+);
