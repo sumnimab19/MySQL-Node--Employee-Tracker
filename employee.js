525414a119c0492f5f23b398e57f3d26d963aa21
Sumnima B <sumnimabhandari@gmail.com>
2020-06-20 21:14:33 -0700

    Added switch cases for main options and added few functions and some empty functions

--- employee.js
+++ employee.js
@@ -31,28 +31,112 @@ function start(){
             "View All Roles"
         ]
     })
-    
-    .then(function(answer){
-        console.log(answer)
-        if (answer.action === "View all Employees") {
-            connection.query ("SELECT * FROM employees", (err,res) => {
-            if(err) throw err;
-            console.log(res);
-        });
-        } else if (answer.action === "View all Employees by Department") {
-            const query = "SELECT * FROM employees e JOIN roles r ON (r.id = e.role_id) JOIN department d ON (r.department_id=d.id)";            
-            connection.query (query, (err,res) => {
-            if(err) throw err;
-            console.log(res);
-            connection.end();
-        });
-        } else if (answer.action === "View all Employees by Manager") {
-            const query = "SELECT * FROM employees e JOIN roles r ON (r.id = e.role_id) JOIN department d ON (r.department_id=d.id)";            
-            connection.query (query, (err,res) => {
-            if(err) throw err;
-            console.log(res);
-            connection.end();
-        });
-    }
+    .then(function(answer) {
+        switch (answer.action) {
+        case "View All Employees":
+          allEmployeeSearch();
+          break;
+  
+        case "View All Employees By Department":
+          employeeByDeptSearch();
+          break;
+  
+        case "FView all Employees by Manager":
+          employeeByManagerSearch();
+          break;
+  
+        case "Add Employee":
+          addEmployee();
+          break;
+  
+        case "Remove Employee":
+          removeEmployee();
+          break;
+        
+        case "Update Employee Role":
+            updateEmployee();
+            break;
+
+        case "Update Employee Manager":
+            updateEmpManager();
+            break;
+
+        case "View All Roles":
+            viewAllRoles();
+            break;
+        }
     });
 }
+    
+function allEmployeeSearch(){
+    const query = "SELECT * FROM employees";
+    connection.query (query, (err,res) => {
+        if(err) throw err;
+        console.table(res);
+    });
+}
+
+
+function employeeByDeptSearch(){
+    const query = "SELECT * FROM employees e JOIN roles r ON (r.id = e.role_id) JOIN department d ON (r.department_id=d.id)";            
+    connection.query (query, (err,res) => {
+        if(err) throw err;
+        console.log(res);
+    });
+}
+            
+function employeeByManagerSearch(){
+    const query = "SELECT * FROM employees e JOIN roles r ON (r.id = e.role_id) JOIN department d ON (r.department_id=d.id)";            
+    connection.query (query, (err,res) => {
+        if(err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
+            
+function addEmployee(){
+    // What is the employee's first name?
+    // What is the employee's last name?
+    // What is employee's role?
+    // list of roles goes here
+    // Who is employee's manager?
+    // List of manager goes here
+    // console.log(Added Employee's First Name + Employee's Last Name to the database)
+
+}
+
+function removeEmployee() {
+    inquirer
+    .prompt({
+      name: "employee",
+      type: "input",
+      message: "Which employee would you like to remove?"
+    })
+    .then(function(answer) {
+        // list the first name of employees so selection can be made to delete it
+      const query = "DELETE * FROM employees ?";
+      connection.query(query, { employee: answer.first_name }, function(err, res) {
+        console.log("Removed employee from database");
+        console.table(res);   
+        start();
+      });
+    });
+}
+
+function updateEmployee() {
+
+}
+
+function updateEmpManager() {
+    // Which employee's manager do you want to update?
+    // List of names
+    // Which employee do you want to set as manager for the selected employee?
+    // List of names
+    // console.log("Updated employee's manager")
+
+}
+
+function viewAllRoles() {
+
+}
+
